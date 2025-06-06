5e391ab9e9f5b92b38c1420374ab756005c5c7d2
deywsjhow <jonathan.deyws@aluno.faculdadeimpacta.com.br>
2025-06-06 19:55:08 -0300

    Versão inicial worker

--- DesafioItau.InvestimentosApp.WorkerKafka/Worker.cs
+++ DesafioItau.InvestimentosApp.WorkerKafka/Worker.cs
@@ -1,24 +1,67 @@
+using Confluent.Kafka;
+using Microsoft.Extensions.Configuration;
+
 namespace DesafioItau.InvestimentosApp.CotacoesConsumer
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly IConsumer<Ignore, string> _consumer;
+        private readonly string _topic;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, IConfiguration configuration)
         {
             _logger = logger;
+            var kafkaConfig = configuration.GetSection("Kafka");
+
+            var consumerConfig = new ConsumerConfig
+            {
+                BootstrapServers = kafkaConfig.GetValue<string>("BootstrapServers"),
+                GroupId = kafkaConfig.GetValue<string>("GroupId"),
+                AutoOffsetReset = AutoOffsetReset.Earliest,
+                EnableAutoCommit = kafkaConfig.GetValue<bool>("EnableAutoCommit")
+            };
+
+            _topic = kafkaConfig.GetValue<string>("Topic");
+
+            _consumer = new ConsumerBuilder<Ignore, string>(consumerConfig).Build();
+            _consumer.Subscribe(_topic);
+
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            while (!stoppingToken.IsCancellationRequested)
+            _logger.LogInformation("Worker iniciado...");
+
+            try
             {
-                if (_logger.IsEnabled(LogLevel.Information))
+                while (!stoppingToken.IsCancellationRequested)
                 {
-                    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                    try
+                    {
+                        var consumeResult = _consumer.Consume(stoppingToken);
+                        _logger.LogInformation("Mensagem recebida {msg}:", consumeResult.Message.Value);
+
+                        //commitando o resultado!
+                        _consumer.Commit(consumeResult);
+                    }
+                    catch (ConsumeException ex)
+                    {
+                        _logger.LogError("Erro ao consumir mensagem {mensagem}:", ex.Error.Reason);
+                    }
                 }
-                await Task.Delay(1000, stoppingToken);
+
             }
+            catch (OperationCanceledException)
+            {
+                _logger.LogInformation("Worker finalizado pelo cancelamento");
+            }
+            finally
+            {
+                _consumer.Close();
+            }
+
+            await Task.CompletedTask;
         }
     }
 }
